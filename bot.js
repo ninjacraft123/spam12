b3a3855426d0e0f5fe962b73e5742fcc55624fa5
ninjacraft123 <42971687+ninjacraft123@users.noreply.github.com>
2018-11-23 01:37:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "515288067016491009"; // ايدي السررفر
+var channel = "515288180837187584";//  ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
